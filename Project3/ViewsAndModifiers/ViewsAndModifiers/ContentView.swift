75a70eaaabc8c9eead841b88a62c186f32a58dfd
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-07 20:53:30 -0300

    Project 3: ViewsAndModifiers. What is behind the main SwiftUI view? Complete

---
+++ Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Ian McDonald on 07/01/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.red)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
