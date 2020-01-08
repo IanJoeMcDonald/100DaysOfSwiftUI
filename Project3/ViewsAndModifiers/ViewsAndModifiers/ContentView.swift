66eb0f172aa7e3e43f6880cc3fd9c3728ef9e444
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-07 21:08:16 -0300

    Project 3: ViewsAndModifiers. View composition Complete

--- Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,16 +9,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    let motto1 = Text("Draco dormiens")
-    let motto2 = Text("nunquam titillandus")
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+                .foregroundColor(.white)
+            CapsuleText(text: "Second")
+                .foregroundColor(.yellow)
+        }
+    }
+}
+
+struct CapsuleText: View {
+    var text: String
     
     var body: some View {
-        VStack {
-            motto1
-                .foregroundColor(.red)
-            motto2
-                .foregroundColor(.blue)
-        }
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(Color.blue)
+            .clipShape(Capsule())
     }
 }
 
