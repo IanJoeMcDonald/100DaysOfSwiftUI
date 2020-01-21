44dee4dabaea79eebb63641031721207c1a28502
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-20 21:39:20 -0300

    Project 7: iExpense. Challenge 1 Complete

--- Project7/iExpense/iExpense/ContentView.swift
+++ Project7/iExpense/iExpense/ContentView.swift
@@ -31,7 +31,8 @@ struct ContentView: View {
                 .onDelete(perform: removeItems)
             }
             .navigationBarTitle("iExpense")
-            .navigationBarItems(trailing:
+            .navigationBarItems(leading: EditButton(),
+                                trailing:
                 Button(action : {
                     self.showingAddExpense = true
                 }) {
