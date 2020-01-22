14f45a082bf918ade3d9f2dc07445c0befcf934a
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-21 22:11:40 -0300

    Project 8: Moonshot. Challenge 3 Complete

--- Project8/Moonshot/Moonshot/ContentView.swift
+++ Project8/Moonshot/Moonshot/ContentView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingDates = true
     
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
@@ -24,12 +25,35 @@ struct ContentView: View {
                     VStack(alignment: .leading) {
                         Text(mission.displayName)
                             .font(.headline)
-                        Text(mission.formatterLaunchDate)
+                        Text(self.showingDates ? mission.formatterLaunchDate : self.crewNames(mission: mission.id))
                     }
                 }
             }
             .navigationBarTitle("Moonshot")
+            .navigationBarItems(trailing:
+                HStack {
+                    Spacer()
+                    Button(showingDates ? "Crew Names" : "Date") {
+                        self.showingDates.toggle()
+                    }
+            })
         }
+    }
+    
+    func crewNames(mission: Int) -> String {
+        guard let missionInfo = missions.first(where: { $0.id == mission}) else { return "" }
+        var first = true
+        var returnString = ""
+        for crew in missionInfo.crew {
+            if(!first) {
+                returnString += ", "
+            }
+            first = false
+            let astronaut = astronauts.first(where: { $0.id == crew.name })
+            returnString += astronaut?.name ?? ""
+        }
+        
+        return returnString
     }
 }
 
