26805c1177a1ea54a5034e9b3b55d58f8988af31
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-22 20:35:17 -0300

    Project 9: Drawing. Adding strokeBorder() support with InsettableShape

--- Project9/Drawing/Drawing/ContentView.swift
+++ Project9/Drawing/Drawing/ContentView.swift
@@ -11,24 +11,30 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Arc(startAngle: .degrees(0), endAngle: .degrees(220), clockwise: true)
-            .stroke(Color.blue, lineWidth: 10)
-            .frame(width: 300, height: 300)
+            .strokeBorder(Color.blue, lineWidth: 40)
     }
 }
 
-struct Arc: Shape {
+struct Arc: InsettableShape{
     var startAngle: Angle
     var endAngle: Angle
     var clockwise: Bool
+    var insetAmount: CGFloat = 0
     
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
         let modifiedStart = startAngle - rotationAdjustment
         let modifiedEnd = endAngle - rotationAdjustment
         var path = Path()
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width/2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width/2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
         
         return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
     }
 }
 
