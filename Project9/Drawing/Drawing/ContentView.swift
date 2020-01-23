45b7e7c91327d36da58ac5983f2e162a45be53a2
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-22 21:34:55 -0300

    Project 9: Drawing. Animating simple shapes with animatableData

--- Project9/Drawing/Drawing/ContentView.swift
+++ Project9/Drawing/Drawing/ContentView.swift
@@ -9,21 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
-
-    @State private var amount: CGFloat = 0.0
+    @State private var insetAmount: CGFloat = 50.0
     
     var body: some View {
-        VStack {
-            Image("PaulHudson")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 200, height: 200)
-                .saturation(Double(amount))
-                .blur(radius: (1 - amount) * 20)
-            
-            Slider(value: $amount)
-                .padding()
+        Trapezoid(insetAmount: insetAmount)
+            .frame(width: 200, height: 200)
+            .onTapGesture {
+                withAnimation {
+                    self.insetAmount = CGFloat.random(in: 10...90)
+                }
         }
+    }
+}
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
     }
 }
 
