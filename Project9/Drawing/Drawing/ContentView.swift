13585cb651010b85e6ef0c6ba2ac170d333ded19
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-22 21:26:31 -0300

    Project 9: Drawing. Special effects in SwiftUI: blurs, blending and more

--- Project9/Drawing/Drawing/ContentView.swift
+++ Project9/Drawing/Drawing/ContentView.swift
@@ -9,44 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var colorCycle = 0.0
-    
-    var body: some View {
-        VStack{
-            ColorCyclingCircle(amount: self.colorCycle)
-                .frame(width: 300, height: 300)
-            
-            Slider(value: $colorCycle)
-        }
-    }
-}
 
-struct ColorCyclingCircle: View {
-    var amount = 0.0
-    var steps = 100
+    @State private var amount: CGFloat = 0.0
     
     var body: some View {
-        ZStack {
-            ForEach(0..<steps) { value in
-                Circle()
-                    .inset(by: CGFloat(value))
-                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
-                        self.color(for: value, brightness: 1),
-                        self.color(for: value, brightness: 0.5)
-                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
-            }
+        VStack {
+            Image("PaulHudson")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .saturation(Double(amount))
+                .blur(radius: (1 - amount) * 20)
+            
+            Slider(value: $amount)
+                .padding()
         }
-        .drawingGroup()
-    }
-    
-    func color(for value: Int, brightness: Double) -> Color {
-        var targetHue = Double(value) / Double(self.steps) + self.amount
-        
-        if targetHue > 1 {
-            targetHue -= 1
-        }
-        
-        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
