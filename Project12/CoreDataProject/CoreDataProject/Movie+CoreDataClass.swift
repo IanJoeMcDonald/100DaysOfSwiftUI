254d9622689563910517379109ddd7e5e5a8d648
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-25 16:53:07 -0300

    Project 12: CoreDataProject. Creating NSManagedObject subclasses

---
+++ Project12/CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Ian McDonald on 25/01/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
---
+++ Project12/CoreDataProject/CoreDataProject/Movie+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Movie+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Ian McDonald on 25/01/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Movie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var director: String?
+    @NSManaged public var year: Int16
+
+    public var wrappedTitle: String {
+        title ?? "Unknown Title"
+    }
+    
+}
