9a53c2b2f3b593ba52ca18bb06b97e05a19e68de
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-25 16:45:01 -0300

    Project 12: CoreDataProject. Why does \.self work for ForEach?

---
+++ Project12/CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Ian McDonald on 25/01/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+
+import SwiftUI
+
+struct Student: Hashable {
+    let name: String
+}
+
+struct ContentView: View {
+    let students = [Student(name: "Harry Potter"), Student(name: "Hermione Granger")]
+    var body: some View {
+        List(students, id: \.self) { student in
+            Text(student.name)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
