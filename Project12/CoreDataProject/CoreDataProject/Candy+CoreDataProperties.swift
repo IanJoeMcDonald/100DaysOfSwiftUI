c1f21eaba86110078d9f49149d62646ac881ef5f
Ian McDonald <ianjoemcdonald@gmail.com>
2020-01-27 21:37:45 -0300

    Project 12: CoreDataProject. One-to-many relationships with Core Data, SwiftUI and @FetchRequest

---
+++ Project12/CoreDataProject/CoreDataProject/Candy+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Candy+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Ian McDonald on 27/01/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Candy {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Candy> {
+        return NSFetchRequest<Candy>(entityName: "Candy")
+    }
+
+    @NSManaged public var name: String?
+    @NSManaged public var origin: Country?
+    
+    public var wrappedName: String {
+        name ?? "Unknown Candy"
+    }
+
+}
---
+++ Project12/CoreDataProject/CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Ian McDonald on 27/01/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
