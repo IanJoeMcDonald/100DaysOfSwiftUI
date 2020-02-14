14dbecab8fdd6938afbac1f3d51ede5791243008
Ian McDonald <ianjoemcdonald@gmail.com>
2020-02-13 21:44:11 -0300

    Project 14: BucketList. Tutorial Complete

---
+++ Project14/BucketList/BucketList/MKPointAnnotation-ObservableObject.swift
@@ -0,0 +1,31 @@
+//
+//  MKPointAnnotation-ObservableObject.swift
+//  BucketList
+//
+//  Created by Ian McDonald on 14/02/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+
+import MapKit
+
+extension MKPointAnnotation: ObservableObject {
+    public var wrappedTitle: String {
+        get {
+            self.title ?? "Unknown value"
+        }
+        
+        set {
+            title = newValue
+        }
+    }
+    
+    public var wrappedSubtitle: String {
+        get {
+            self.subtitle ?? "Unknown value"
+        }
+        
+        set {
+            subtitle = newValue
+        }
+    }
+}
