1306ef873bde171d093246cab14b33f0e7067617
Ian McDonald <ianjoemcdonald@gmail.com>
2020-02-22 12:42:07 -0300

    Project 15: Accessibility. Challenge 3 Complete.

---
+++ Project15/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Ian McDonald on 21/01/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
