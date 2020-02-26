ddadc83ef43fdb38b06372730781bb0f7bcc53b1
Ian McDonald <ianjoemcdonald@gmail.com>
2020-02-26 18:03:18 -0300

    Project 16: Hot Prospects. Challenge 1 Complete

--- Project16/HotProspects/HotProspects/ProspectsView.swift
+++ Project16/HotProspects/HotProspects/ProspectsView.swift
@@ -46,11 +46,17 @@ struct ProspectsView: View {
         NavigationView {
             List {
                 ForEach(filteredProspects) { prospect in
-                    VStack(alignment: .leading) {
-                        Text(prospect.name)
-                            .font(.headline)
-                        Text(prospect.emailAddress)
-                            .foregroundColor(.secondary)
+                    HStack {
+                        if self.filter == .none {
+                            Image(systemName: prospect.isContacted ? "checkmark.circle" :
+                                "questionmark.diamond")
+                        }
+                        VStack(alignment: .leading) {
+                            Text(prospect.name)
+                                .font(.headline)
+                            Text(prospect.emailAddress)
+                                .foregroundColor(.secondary)
+                        }
                     }
                     .contextMenu {
                         Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted") {
