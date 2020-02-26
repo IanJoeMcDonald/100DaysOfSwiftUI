d02f4968a8fd28cacd0564fc8d6e54c90cc0206f
Ian McDonald <ianjoemcdonald@gmail.com>
2020-02-26 17:52:50 -0300

    Project 16: Hot Prospects. Tutorial Complete

---
+++ Project16/HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,48 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Masipack Eletronica on 26/02/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+
+import Foundation
+
+class Prospect: Identifiable, Codable {
+    let id = UUID()
+    var name = "Anonymos"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    @Published private(set) var people: [Prospect]
+    static let saveKey = "SavedData"
+    init() {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
+        
+        self.people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+}
