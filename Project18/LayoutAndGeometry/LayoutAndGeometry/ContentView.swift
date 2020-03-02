4669765c8a0ce1468eb537f4b5df0beeed9c7084
Ian McDonald <ianjoemcdonald@gmail.com>
2020-03-02 16:29:21 -0300

    Project 18: Layout & Geometry. How layout works in SwiftUI.

---
+++ Project18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Ian McDonald on 02/03/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Color.red
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
