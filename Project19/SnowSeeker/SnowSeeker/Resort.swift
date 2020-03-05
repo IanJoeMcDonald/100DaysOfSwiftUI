50bf2e8b017a16875e3cac85c34619a83acb959e
Ian McDonald <ianjoemcdonald@gmail.com>
2020-03-05 16:07:12 -0300

    Project 19: SnowSeeker. Tutorial Complete

---
+++ Project19/SnowSeeker/SnowSeeker/Resort.swift
@@ -0,0 +1,30 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Masipack Eletronica on 05/03/20.
+//  Copyright © 2020 Ian McDonald. All rights reserved.
+//
+
+import Foundation
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+    
+    var facilityTypes: [Facility] {
+        facilities.map(Facility.init)
+    }
+    
+    static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
+    static let example = allResorts[0]
+}
